0d377293df1f114e3f754b864afd324321345b69
theabroadwolf <belalgamal00@gmail.com>
2023-07-30 14:01:09 +0300

    Add users to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(name: 'belal', user_name: 'belalgamal46', email: 'belalgamal00@gmail.com', password: '123456', role: 'admin')
+User.create(name: 'abdallah', user_name: 'abdallah', email: 'abdallah@gmail.com', password: '123456', role: 'manager')
+User.create(name: 'ahmed', user_name: 'ahmed456', email: 'ahmed@gmail.com', password: '123456')
