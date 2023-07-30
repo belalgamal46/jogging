4f93511875db002d703e3c8e3a1a5280e9365121
theabroadwolf <belalgamal00@gmail.com>
2023-07-30 13:08:48 +0300

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_26_065830) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_29_184851) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "jogging_sessions", force: :cascade do |t|
+    t.integer "distance_in_meters", null: false
+    t.float "duration_in_minutes", null: false
+    t.datetime "jogging_started_at", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_jogging_sessions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "user_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "jti", null: false
+    t.string "role", default: "regular"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "jogging_sessions", "users"
 end
