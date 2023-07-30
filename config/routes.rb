e29c8cfc42ed9a01db5a4b61b3db2fa3f3723fb4
theabroadwolf <belalgamal00@gmail.com>
2023-07-30 13:09:04 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/users', to: 'users/users#index'
+  put '/users/:id', to: 'users/users#update'
+  patch '/users/:id', to: 'users/users#update'
+  delete '/users/:id', to: 'users/users#destroy'
+
+
+  namespace :api do
+    namespace :v1 do
+      resources :jogging_sessions
+      get '/report/:period', to: 'jogging_sessions#report'
+    end
+  end
+
 end
